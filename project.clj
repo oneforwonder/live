1138ef73c37b91e1ad842f49f1f892162ce08b82
Alex Ponting <happy.ajp050789@gmail.com>
2014-07-04 15:05:52 -0700

    surely no conflicts will arise

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject live "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Live, dammit!"
+  :url "http://github.com/rjn945/live"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [om "0.6.4"]])
