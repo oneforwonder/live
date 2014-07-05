8bafa11a66cb85ddf7c39f3e8e79430c7ef3f391
Rob Nagle <rjn945@gmail.com>
2014-07-04 18:30:05 -0700

    Added printlns

--- src/live/core.clj
+++ src/live/core.clj
@@ -5,9 +5,11 @@
 (def count (atom 124))
 
 (defremote get-count  []
+  (println "in server's get-count")
   (identity @count))
 
 (defremote set-count  [n]
+  (println "in server's set-count")
   (set! count n))
 
 (defn -main [& args]
